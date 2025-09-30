498b8e8af92e259b040e7cf363760d7c1f641511
Emilia Serafin <113944669+Lionp10@users.noreply.github.com>
2025-09-30 11:13:50 -0300

    Migracion entidad Lifetime, ajustes estilos en PlayerStats.cshtml

---
+++ TNA.DAL/Migrations/20250930135938_Add_LifetimeStatsEntity.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TNA.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_LifetimeStatsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayerLifetimes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerId = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    DateOfUpdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LifetimeJson = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerLifetimes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerLifetimes_PlayerId",
+                table: "PlayerLifetimes",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerLifetimes");
+        }
+    }
+}
